75ab7296b92a049cccd46161dc00dedda8e39bd9
tracer-nju <tracer@nju.edu.cn>
2021-09-29 18:46:47 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 7ab7dfa1fea8430d932787f54b5523cf     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  18:46:47 up  5:35,  1 user,  load average: 0.03, 0.04, 0.01

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -5,7 +5,7 @@
 void multiply_128(uint64_t, uint64_t, uint64_t*, uint64_t*);
 void module_128(uint64_t*, uint64_t*, uint64_t);
 void srl_128(uint64_t*, uint64_t*, bool);
-bool add_128(uint64_t*, uint64_t*, unsigned);
+bool add_128(uint64_t*, unsigned);
 void sub_128(uint64_t*, uint64_t*, unsigned);
 
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
@@ -43,15 +43,15 @@ void multiply_128(uint64_t a, uint64_t b, uint64_t *hres, uint64_t *lres) {
 
 void srl_128(uint64_t *h_64, uint64_t *l_64, bool cin) {
 	*l_64 >>= 1;
-	if ((*l_64&0x1)== 1) {
+	if ((*h_64&0x1)== 1) {
 		*l_64 |= 0x1000000000000000;
 	}
 	*h_64 >>= 1;
 	if (cin) {*h_64 |= 0x1000000000000000;}	
 }
 
-bool add_128(uint64_t *h_64, uint64_t *l_64, unsigned num) {
-	uint64_t temp = *l_64;
+bool add_128(uint64_t *h_64, unsigned num) {
+	uint64_t temp = *h_64;
 	*h_64 += num;
 	if (*h_64 < temp) {
 		return 1;
