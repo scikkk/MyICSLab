bdd19e5cfb6e71e140ad8637d5c76a779e656afc
tracer-nju <tracer@nju.edu.cn>
2021-09-29 20:11:58 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 7ab7dfa1fea8430d932787f54b5523cf     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  20:11:59 up  7:00,  1 user,  load average: 0.08, 0.06, 0.07

--- multimod/main.c
+++ multimod/main.c
@@ -5,13 +5,18 @@
 uint64_t multimod(uint64_t, uint64_t, uint64_t);
 void py_mod(uint64_t, uint64_t, uint64_t);
 void test(uint64_t a, uint64_t b, uint64_t m) {
-  py_mod(a,b,m);
-  printf("(" U64 " * " U64 ") mod " U64 " = " U64 "\n\n", a, b, m, multimod(a, b, m));
+	py_mod(a,b,m);
+	printf("(" U64 " * " U64 ") mod " U64 " = " U64 "\n\n", a, b, m, multimod(a, b, m));
 }
 
 int main() {
 	test(3,6,1000);
-   test(123, 456, 789);
-  test(123, 456, -1ULL);
-  test(-2ULL, -2ULL, -1ULL); // should be 1
+	test(123, 456, 789);
+	test(-2ULL, 4, 415); // should be 1
+	test(14258, -2ULL, 1425); // should be 1
+	test(123, 456, -1ULL);
+	test(41258, -2ULL, -1ULL); // should be 1
+	test(-2ULL, 102485, -1ULL); // should be 1
+	test(-2ULL, -2ULL, 142558); // should be 1
+	test(-2ULL, -2ULL, -1ULL); // should be 1
 }
