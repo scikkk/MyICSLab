7352e13910444e76352515ef9ab8aa47dac07ca4
tracer-nju <tracer@nju.edu.cn>
2021-09-29 21:33:22 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 7ab7dfa1fea8430d932787f54b5523cf     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  21:33:22 up  8:21,  1 user,  load average: 0.18, 0.05, 0.01

--- multimod/main.c
+++ multimod/main.c
@@ -5,8 +5,9 @@
 uint64_t multimod(uint64_t, uint64_t, uint64_t);
 void py_mod(uint64_t, uint64_t, uint64_t);
 void test(uint64_t a, uint64_t b, uint64_t m) {
-	py_mod(a,b,m);
 	printf("(" U64 " * " U64 ") mod " U64 " = " U64 "\n\n", a, b, m, multimod(a, b, m));
+
+	py_mod(a,b,m);
 }
 
 int main() {
