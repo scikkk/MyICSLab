0223a3b975c069c428c90bf26305c73e31b1ab9c
tracer-nju <tracer@nju.edu.cn>
2021-09-29 17:01:00 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 7ab7dfa1fea8430d932787f54b5523cf     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  17:01:00 up  3:49,  1 user,  load average: 0.14, 0.10, 0.06

---
+++ multimod/test.c
@@ -0,0 +1,22 @@
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+void py_mod(uint64_t a,uint64_t b, uint64_t m ){
+	char exec[120];
+	sprintf(exec, "python3 -c 'print((%lu*%lu)%%%lu)'", a,b,m);
+	printf("%s\n",exec);
+	FILE *fp = popen(exec, "r");
+	assert(fp);
+	char buf[100];
+	fscanf(fp, "%s", buf);
+	printf("py_mod returns: %s\n\n", buf);
+	pclose(fp);
+	/* FILE *fp = popen("python3 -c 'print(10**10 - 1)'", "r"); */
+	/* assert(fp); */
+	/* char buf[100]; */
+	/* fscanf(fp, "%s", buf); */
+	/* printf("popen() returns: %s\n", buf); */
+	/* pclose(fp); */
+}
