def896af27848b5188aa6c2c58fe9e198ae8f622
tracer-nju <tracer@nju.edu.cn>
2021-09-29 18:18:43 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 7ab7dfa1fea8430d932787f54b5523cf     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  18:18:43 up  5:07,  1 user,  load average: 0.03, 0.05, 0.01

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,5 +1,65 @@
 #include <stdint.h>
+#include <stdlib.h>
+
+void multiply_128(uint64_t, uint64_t, uint64_t*, uint64_t*);
+void module_128(uint64_t*, uint64_t*, uint64_t);
+void sll_128(uint64_t*, uint64_t*, unsigned);
+void add_128(uint64_t*, uint64_t*, unsigned);
+void sub_128(uint64_t*, uint64_t*, unsigned);
 
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
-  return (a * b) % m; // buggy
+  uint64_t *h_64 = (uint64_t*)malloc(8);
+  uint64_t *l_64 = (uint64_t*)malloc(8);
+  multiply_128(a, b, h_64, l_64);
+  module_128(h_64, l_64, m);
+  // return (a * b) % m; // buggy
+  return *l_64;
+}
+
+void module_128(uint64_t* h_64, uint64_t* l_64, uint64_t m){
+	uint64_t temp_l;
+	while(*h_64 != 0) {
+		temp_l = *l_64;
+		*l_64 -= m;
+		if (*l_64 > temp_l) {
+			*h_64 -= 1;
+		}
+	}
+}
+
+void multiply_128(uint64_t a, uint64_t b, uint64_t *hres, uint64_t *lres) {
+	*hres &= 0x0;
+	*lres = b;
+	for (int k = 0; k < 32; k++) {
+	   if ((*lres&0x1) == 1) {
+			add_128(hres, lres, a);
+	   }
+	   sll_128(hres,lres,1);
+	}
+}
+
+void sll_128(uint64_t *h_64, uint64_t *l_64, unsigned num) {
+	for (unsigned k = 0; k < num; k++){
+		*h_64 <<= 1;
+		if ((*l_64|0x8000000000000000) != 0) {
+			*h_64 |= 0x1;
+		}
+		*l_64 <<= 1;
+	}
+}
+
+void add_128(uint64_t *h_64, uint64_t *l_64, unsigned num) {
+	uint64_t temp = *l_64;
+	*l_64 += num;
+	if (*l_64 < temp) {
+		*h_64 += 1;
+	}
+}
+
+void sub_128(uint64_t *h_64, uint64_t *l_64, unsigned num) {
+	uint64_t temp = *l_64;
+	*l_64 -= num;
+	if (*l_64 > temp) {
+		*h_64 -= 1;
+	}
 }
