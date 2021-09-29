15a4f9deed6aa966ade48d718d012fe15dbc2a34
tracer-nju <tracer@nju.edu.cn>
2021-09-29 18:21:25 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 7ab7dfa1fea8430d932787f54b5523cf     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  18:21:25 up  5:09,  1 user,  load average: 0.06, 0.06, 0.01

--- multimod/main.c
+++ multimod/main.c
@@ -10,7 +10,8 @@ void test(uint64_t a, uint64_t b, uint64_t m) {
 }
 
 int main() {
-  test(123, 456, 789);
-  test(123, 456, -1ULL);
-  test(-2ULL, -2ULL, -1ULL); // should be 1
+	test(10,1,3);
+  // test(123, 456, 789);
+  // test(123, 456, -1ULL);
+  // test(-2ULL, -2ULL, -1ULL); // should be 1
 }
