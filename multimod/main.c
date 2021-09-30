7dfb6f7736f7365b5eb1f43cd24e82fd7979ed2d
tracer-nju <tracer@nju.edu.cn>
2021-09-30 15:35:20 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 62fa6723d99e48d0b9a52c21bcc864b1     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  15:35:20 up  2:07,  1 user,  load average: 0.16, 0.19, 0.11

--- multimod/main.c
+++ multimod/main.c
@@ -9,16 +9,18 @@
 uint64_t multimod(uint64_t, uint64_t, uint64_t);
 void py_mod(uint64_t, uint64_t, uint64_t, char*);
 void test(uint64_t a, uint64_t b, uint64_t m) {
-	char right[99], mymod[99];
+	char right[99];
+  // char 	mymod[99];
 	py_mod(a,b,m,right);
 	uint64_t myres = multimod(a, b, m);
-	sprintf(mymod, "%lu", myres);
+//	sprintf(mymod, "%lu", myres);
 //	if (strcmp(right, mymod) != 0) {
 //		printf("Failure!");
 //		printf("right: %s\nmymod: %s\n", right, mymod);
 //	} 
-		printf("(%lu * %lu) %% %lu \nright: %s\nmymod: %s\n\n", a, b, m, right, mymod);
-//	printf("(" U64 " * " U64 ") mod " U64 " = " U64 "\n\n", a, b, m, multimod(a, b, m));
+//		printf("(%lu * %lu) %% %lu \nright: %s\nmymod: %s\n\n", a, b, m, right, mymod);
+	printf("(" U64 " * " U64 ") mod " U64 " = %s\n\n", a, b, m, right);
+	printf("(" U64 " * " U64 ") mod " U64 " = " U64 "\n\n", a, b, m, myres);
 }
 
 int main() {
