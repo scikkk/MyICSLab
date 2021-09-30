dfc70956a840d9e74366fe737a97662f2a09b773
tracer-nju <tracer@nju.edu.cn>
2021-09-30 08:52:24 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 9ae9154494ef40b1a984749304830e70     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  08:52:24 up 9 min,  1 user,  load average: 0.00, 0.48, 0.47

--- multimod/main.c
+++ multimod/main.c
@@ -12,22 +12,22 @@ void test(uint64_t a, uint64_t b, uint64_t m) {
 }
 
 int main() {
-	test(201300008, 13851256176, -1ULL); 
-	test(12014254782, 203536673, -1ULL); 
-	test(4102155485214, 1425, -1ULL); 
-	test(2145, 1916145145, -1ULL); 
-	test(1452288, 5, 2013); 
-	test(2, -2ULL, -1ULL); 
-	test(-2ULL, -2ULL, 142558);
-	test(14258, -2ULL, 1425); 
-	test(1452, -2ULL, -1ULL); 
-	test(123, 456, -1ULL);
-	test(41258, -2ULL, -1ULL);
-	test(-2ULL, 14, -1ULL); 
-	test(-2ULL, 414251455, -1ULL); 
-	test(14521452145214, 1452142145, -1ULL); 
-	test(12451145243, 441487522526, -1ULL);
-	test(-2ULL, 4, 415); 
 	test(-2ULL, 102485, -1ULL);
-	test(-2ULL, -2ULL, -1ULL); // should be 1
+	/* test(201300008, 13851256176, -1ULL); */ 
+	/* test(12014254782, 203536673, -1ULL); */ 
+	/* test(4102155485214, 1425, -1ULL); */ 
+	/* test(2145, 1916145145, -1ULL); */ 
+	/* test(1452288, 5, 2013); */ 
+	/* test(2, -2ULL, -1ULL); */ 
+	/* test(-2ULL, -2ULL, 142558); */
+	/* test(14258, -2ULL, 1425); */ 
+	/* test(1452, -2ULL, -1ULL); */ 
+	/* test(123, 456, -1ULL); */
+	/* test(41258, -2ULL, -1ULL); */
+	/* test(-2ULL, 14, -1ULL); */ 
+	/* test(-2ULL, 414251455, -1ULL); */ 
+	/* test(14521452145214, 1452142145, -1ULL); */ 
+	/* test(12451145243, 441487522526, -1ULL); */
+	/* test(-2ULL, 4, 415); */ 
+	/* test(-2ULL, -2ULL, -1ULL); // should be 1 */
 }
