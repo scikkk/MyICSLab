8135a5259062c62840dd1650eba35844bca93ec9
tracer-nju <tracer@nju.edu.cn>
2021-09-30 15:07:55 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 62fa6723d99e48d0b9a52c21bcc864b1     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  15:07:55 up  1:39,  1 user,  load average: 0.24, 0.16, 0.10

--- multimod/main.c
+++ multimod/main.c
@@ -5,19 +5,19 @@
 #include <string.h>
 #include <time.h>
 #define U64 "%" PRIu64
-#define NUM 1000
+#define NUM 10
 uint64_t multimod(uint64_t, uint64_t, uint64_t);
 void py_mod(uint64_t, uint64_t, uint64_t, char*);
 void test(uint64_t a, uint64_t b, uint64_t m) {
 	char right[99], mymod[99];
 	py_mod(a,b,m,right);
 	uint64_t myres = multimod(a, b, m);
-	sprintf(mymod, "%lu", myres);
-	if (strcmp(right, mymod) != 0) {
-		printf("Failure!");
-		printf("right: %s\nmymod: %s\n", right, mymod);
-	} 
-	//	printf("right: %s\nmymod: "  U64 "\n", right,myres);
+//	sprintf(mymod, "%lu", myres);
+//	if (strcmp(right, mymod) != 0) {
+//		printf("Failure!");
+//		printf("right: %s\nmymod: %s\n", right, mymod);
+//	} 
+		printf("right: %s\nmymod: "  U64 "\n", right,myres);
 	//	printf("(" U64 " * " U64 ") mod " U64 " = " U64 "\n\n", a, b, m, multimod(a, b, m));
 }
 
