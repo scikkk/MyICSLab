8ec72243994bff8336c5b64006c62a689aa8f500
tracer-nju <tracer@nju.edu.cn>
2021-09-30 14:14:15 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 62fa6723d99e48d0b9a52c21bcc864b1     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  14:14:15 up 46 min,  1 user,  load average: 0.00, 0.02, 0.04

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -65,13 +65,14 @@ void module_128(uint64_t* h_64, uint64_t* l_64, uint64_t m){
 	len_0 = 64 - len;
 	short r_num = 0;
 	while (len_0 >= 0){
+				printf("0x%016lx %016lx\n", *h_64, *l_64);
 		while (*h_64 != 0 || *l_64 >= (m<<len_0)) {
 			temp_l = *l_64;
 			*l_64 -= (m<<len_0);
 			if (*l_64 > temp_l) {
 				*h_64 -= 1;
 			}
-			//	printf("0x%016lx %016lx\n", *h_64, *l_64);
+				printf("0x%016lx %016lx\n", *h_64, *l_64);
 			if ((*l_64 & 0x1) == 0) {
 				srl_128(0,h_64,l_64);
                 r_num++;
