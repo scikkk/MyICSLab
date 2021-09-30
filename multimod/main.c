de9624d2bc6e08d28ce4db1506872f68880fce8d
tracer-nju <tracer@nju.edu.cn>
2021-09-30 15:04:56 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 62fa6723d99e48d0b9a52c21bcc864b1     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  15:04:57 up  1:36,  1 user,  load average: 0.00, 0.04, 0.07

--- multimod/main.c
+++ multimod/main.c
@@ -2,26 +2,34 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <inttypes.h>
+#include <string.h>
 #include <time.h>
 #define U64 "%" PRIu64
 #define NUM 10
 uint64_t multimod(uint64_t, uint64_t, uint64_t);
 void py_mod(uint64_t, uint64_t, uint64_t, char*);
 void test(uint64_t a, uint64_t b, uint64_t m) {
-	char right[99];
+	char right[99], mymod[99];
 	py_mod(a,b,m,right);
-	printf("right: %s\nmymod: "  U64 "\n", right,multimod(a, b, m));
-//	printf("(" U64 " * " U64 ") mod " U64 " = " U64 "\n\n", a, b, m, multimod(a, b, m));
+	uint64_t myres = multimod(a, b, m);
+	sprintf(mymod, "%lu", myres);
+	if (strcmp(right, mymod) != 0) {
+		printf("Failure!");
+		printf("right: %s\nmymod: %s\n", right, mymod);
+
+	} 
+	//	printf("right: %s\nmymod: "  U64 "\n", right,myres);
+	//	printf("(" U64 " * " U64 ") mod " U64 " = " U64 "\n\n", a, b, m, multimod(a, b, m));
 }
 
 int main() {
 	uint64_t a, b, m; 
-    srand((unsigned)time(NULL));
+	srand((unsigned)time(NULL));
 	for (int k = 0; k < NUM ; k++) {
-	a = rand() - 999;
-	b = rand() - 999;
-	m = rand() - 999;
-	test(a, b, m);
+		a = rand() - 999;
+		b = rand() - 999;
+		m = rand() - 999;
+		test(a, b, m);
 	}
 	test(-2ULL, -2ULL, -1ULL); // should be 1
 }
