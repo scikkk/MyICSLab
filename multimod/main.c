21bf7cc5f3d4fd6de8ae324f49190c898b5d0a74
tracer-nju <tracer@nju.edu.cn>
2021-09-30 14:46:28 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 62fa6723d99e48d0b9a52c21bcc864b1     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  14:46:28 up  1:18,  1 user,  load average: 0.00, 0.02, 0.05

--- multimod/main.c
+++ multimod/main.c
@@ -1,32 +1,26 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <inttypes.h>
+#include <time.h>
 #define U64 "%" PRIu64
+#define NUM 100;
 uint64_t multimod(uint64_t, uint64_t, uint64_t);
 void py_mod(uint64_t, uint64_t, uint64_t);
 void test(uint64_t a, uint64_t b, uint64_t m) {
 	py_mod(a,b,m);
 	// printf("(" U64 " * " U64 ") mod " U64 " = " U64 "\n\n", a, b, m, multimod(a, b, m));
-	printf( U64 "\n", multimod(a, b, m));
+	printf( U64 "\n\n", multimod(a, b, m));
 }
 
 int main() {
-	test(-2ULL, 102485, -1ULL);
-	test(201300008, 13851256176, -1ULL); 
-	test(12014254782, 203536673, -1ULL); 
-	test(4102155485214, 1425, -1ULL); 
-	test(2145, 1916145145, -1ULL); 
-	test(1452288, 5, 2013); 
-	test(2, -2ULL, -1ULL); 
-	test(-2ULL, -2ULL, 142558);
-	test(14258, -2ULL, 1425); 
-	test(1452, -2ULL, -1ULL); 
-	test(123, 456, -1ULL);
-	test(41258, -2ULL, -1ULL);
-	test(-2ULL, 14, -1ULL); 
-	test(-2ULL, 414251455, -1ULL); 
-	test(14521452145214, 1452142145, -1ULL); 
-	test(12451145243, 441487522526, -1ULL);
-	test(-2ULL, 4, 415); 
-	test(-2, -2ULL, -1); // should be 1
+	int a, b, m; 
+	for (int k = 0; k < NUM; k++) {
+    srand((unsigned)time(NULL));
+	a = rand();
+	b = rand();
+	m = rand();
+	test(a, b, m);
+	}
+	test(-2ULL, -2ULL, -1ULL); // should be 1
 }
+
