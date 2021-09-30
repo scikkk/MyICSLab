77fef0209e83d5d705b4307770b2a76ae3edabd9
tracer-nju <tracer@nju.edu.cn>
2021-09-30 21:59:56 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: caa47b08fdf746af84a31158f3ca3247     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  21:59:56 up 30 min,  1 user,  load average: 0.13, 0.08, 0.08

--- multimod/test.c
+++ multimod/test.c
@@ -3,15 +3,17 @@
 //#include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
+#define U64 "%" PRIu64
 
 void py_mod(uint64_t a,uint64_t b, uint64_t m , char *res){
 	char exec[120];
-	sprintf(exec, "python3 -c 'print((" U64 "*" U64 ")%%" U64 ")'", a,b,m);
+	/* sprintf(exec, "python3 -c 'print((" U64 "*" U64 ")%%" U64 ")'", a,b,m); */
 //	printf("%s\n",exec);
 	FILE *fp = popen(exec, "r");
 	assert(fp);
 	char buf[100];
 	fscanf(fp, "%s", buf);
+	printf("(" U64 " * " U64 ") mod " U64 " = %s\n\n", a, b, m, buf);
 //	printf("py_mod returns: %s\n", buf);
 //	printf("%s\n", buf);
     strcpy(res, buf);
