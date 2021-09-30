dc23f3644d9549db5bf16685af23b22fbb0172c1
tracer-nju <tracer@nju.edu.cn>
2021-09-30 14:33:46 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 62fa6723d99e48d0b9a52c21bcc864b1     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  14:33:46 up  1:05,  1 user,  load average: 0.09, 0.14, 0.09

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -44,7 +44,7 @@ uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
 	uint64_t *l_64 = (uint64_t*)malloc(8);
 	// printf("0x%016lx %016lx\n", a, b);
 	multiply_128(a, b, h_64, l_64);
-	 printf("0x%016lx %016lx\n", *h_64, *l_64);
+	printf("0x%016lx %016lx\n", *h_64, *l_64);
 	module_128(h_64, l_64, m);
 	// return (a * b) % m; // buggy
 	return *l_64;
@@ -58,31 +58,33 @@ void module_128(uint64_t* h_64, uint64_t* l_64, uint64_t m){
 	while (len_0 >= 0){
 		while (*h_64 >= (m<<len_0)) {
 			*h_64 -= (m<<len_0);
-		//	printf("0x%016lx %016lx\n", *h_64, *l_64);
+			//	printf("0x%016lx %016lx\n", *h_64, *l_64);
 		}
 		len_0--;
 	} 
 	len_0 = 64 - len;
 	short r_num = 0;
 	while (len_0 >= 0){
-	//	printf("0x%016lx %016lx\n", *h_64, *l_64);
+		//	printf("0x%016lx %016lx\n", *h_64, *l_64);
 		while (*h_64 != 0 || *l_64 >= (m<<len_0)) {
 			temp_l = *l_64;
 			*l_64 -= (m<<len_0);
 			if (*l_64 > temp_l) {
 				*h_64 -= 1;
 			}
-		//	printf("0x%016lx %016lx\n", *h_64, *l_64);
+			//	printf("0x%016lx %016lx\n", *h_64, *l_64);
 			if ((*l_64 & 0x1) == 0) {
 				srl_128(0,h_64,l_64);
 				r_num++;
-			//	printf(">>1: 0x%016lx %016lx\n", *h_64, *l_64);
+				//	printf(">>1: 0x%016lx %016lx\n", *h_64, *l_64);
 			}
 		}
-		*h_64 <<= r_num;
-		*h_64 |= *l_64 >> (64 - r_num);
-		*l_64 <<= r_num;
-	//	printf("<<%d: 0x%016lx %016lx\n\n", r_num,*h_64, *l_64);
+		if(r_num >0 )	{
+			*h_64 <<= r_num;
+			*h_64 |= *l_64 >> (64 - r_num);
+			*l_64 <<= r_num;
+		}
+		//	printf("<<%d: 0x%016lx %016lx\n\n", r_num,*h_64, *l_64);
 		r_num = 0;
 		len_0--;
 	}
@@ -92,7 +94,7 @@ void module_128(uint64_t* h_64, uint64_t* l_64, uint64_t m){
 		if (*l_64 > temp_l) {
 			*h_64 -= 1;
 		}
-	//	printf("0x%016lx %016lx\n", *h_64, *l_64);
+		//	printf("0x%016lx %016lx\n", *h_64, *l_64);
 	}
 }
 
@@ -154,7 +156,7 @@ void multiply_128(uint64_t a, uint64_t b, uint64_t *hres, uint64_t *lres) {
 		y0 = y1;
 	}
 
-//	printf("0x%d%d %016lx %016lx %d%d\n",cout2, cout1, *hres, *lres, y1, y0);
+	//	printf("0x%d%d %016lx %016lx %d%d\n",cout2, cout1, *hres, *lres, y1, y0);
 }
 
 void srl_128( bool c1, uint64_t *h_64, uint64_t *l_64) {
