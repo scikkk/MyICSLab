929f551c20cd7f3bd926d3baec936bcbbea6cda3
tracer-nju <tracer@nju.edu.cn>
2021-10-06 21:56:03 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 9e21a9f26c484cdfa3d9c496b7578cf3     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  21:56:04 up  1:15,  1 user,  load average: 0.01, 0.01, 0.00

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,11 +1,11 @@
 #include <stdint.h>
-uint64_t inplacemod(uint64_t a, uint64_t b, uint64_t m) {
-
-	return (a+b)%m;
+uint64_t inplacemod(uint64_t sum, uint64_t m) {
+    
+	return sum%m;
 }
 uint64_t addmod(uint64_t a, uint64_t b, uint64_t m) {
     if (a + b < a || a + b < b) {
-	 uint64_t res =  inplacemod(a, b, m) + inplacemod(-1ULL, 0 , m);
+	 uint64_t res =  inplacemod(a+ b, m) + inplacemod(-1ULL, m);
 	 if (res + 1 < m) {
 	 return res + 1;
 	 }
@@ -14,7 +14,7 @@ uint64_t addmod(uint64_t a, uint64_t b, uint64_t m) {
 	 }
 	}
 	else {
-	return inplacemod(a,b,m);
+	return inplacemod(a+b,m);
 	}
 }
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
