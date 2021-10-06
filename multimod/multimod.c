9bdf1cf75b855c036596439d389e3cf0596805f5
tracer-nju <tracer@nju.edu.cn>
2021-10-06 20:58:01 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 9e21a9f26c484cdfa3d9c496b7578cf3     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  20:58:01 up 17 min,  1 user,  load average: 0.06, 0.15, 0.25

--- multimod/multimod.c
+++ multimod/multimod.c
@@ -1,10 +1,10 @@
-#include <stdbool.h>
+/* #include <stdbool.h> */
 #include <stdint.h>
 #include <stdlib.h>
-#include <stdio.h>
+/* #include <stdio.h> */
 void multiply_128(uint64_t, uint64_t, uint64_t*, uint64_t*);
 void module_128(uint64_t*, uint64_t*, uint64_t);
-void srl_128(bool, uint64_t*, uint64_t*);
+void srl_128(short , uint64_t*, uint64_t*);
 
 uint64_t multimod(uint64_t a, uint64_t b, uint64_t m) {
 	// printf("0x%016lx %016lx\n", a, b);
@@ -102,8 +102,8 @@ void multiply_128(uint64_t a, uint64_t b, uint64_t *hres, uint64_t *lres) {
 	*hres &= 0x0;
 
 	*lres = b;
-	bool cout1 = 0;
-	bool cout2 = 0;
+	short cout1 = 0;
+	short cout2 = 0;
 	short y0 = 0,y1;
 	for (int k = 0; k < 65; k++) {
 		y1 = *lres&0x1;
@@ -117,7 +117,7 @@ void multiply_128(uint64_t a, uint64_t b, uint64_t *hres, uint64_t *lres) {
 			h_63_0 += a;
 			//	printf("h_63_0: %016lx\n",h_63_0);
 			//	if ((temp > h_63_0&&(a>>63)==0x0)||((temp < h_63_0)&&(a>>63)==0x1)) {cout2 = !cout2;}
-			if (temp > h_63_0) {cout2 = !cout2;}
+			if (temp > h_63_0) {cout2 = 1 - cout2;}
 			cout1 = ((h_63_0&0x8000000000000000) == 0x8000000000000000);
 			if ((*hres & 0x1) == 0){
 				*hres = h_63_0;
@@ -138,7 +138,7 @@ void multiply_128(uint64_t a, uint64_t b, uint64_t *hres, uint64_t *lres) {
 			h_63_0 -= a;
 			//	printf("h_63_0: %016lx\n",h_63_0);
 			//	if ((temp < h_63_0&&(a>>63)==0x0)||((temp > h_63_0)&&(a>>63)==0x1)) {cout2 = !cout2;}
-			if (temp < h_63_0) {cout2 = !cout2;}
+			if (temp < h_63_0) {cout2 = 1 - cout2;}
 			cout1 = ((h_63_0&0x8000000000000000) == 0x8000000000000000);
 			if ((*hres & 0x1) == 0){
 				*hres = h_63_0;
@@ -159,13 +159,13 @@ void multiply_128(uint64_t a, uint64_t b, uint64_t *hres, uint64_t *lres) {
 	//	printf("0x%d%d %016lx %016lx %d%d\n",cout2, cout1, *hres, *lres, y1, y0);
 }
 
-void srl_128( bool c1, uint64_t *h_64, uint64_t *l_64) {
+void srl_128( short c1, uint64_t *h_64, uint64_t *l_64) {
 	*l_64 >>= 1;
 	if ((*h_64&0x1)== 1) {
 		*l_64 |= 0x8000000000000000;
 	} 
 	*h_64 >>= 1;
-	if (c1 ){
+	if (c1 == 1 ){
 		*h_64 |= 0x8000000000000000; 
 	}
 }
