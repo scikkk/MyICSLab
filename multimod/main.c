2516275c6ea1cac3c273fc734bb407d5959770e0
tracer-nju <tracer@nju.edu.cn>
2021-10-06 20:48:19 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 9e21a9f26c484cdfa3d9c496b7578cf3     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-37-generic       Architecture: x86-64  20:48:19 up 7 min,  1 user,  load average: 0.26, 0.69, 0.44

--- multimod/main.c
+++ multimod/main.c
@@ -10,29 +10,30 @@ uint64_t multimod(uint64_t, uint64_t, uint64_t);
 void py_mod(char*);
 void test(uint64_t a, uint64_t b, uint64_t m) {
 	char right[99];
- //  char 	mymod[99];
+	//  char 	mymod[99];
 	sprintf(right, "python3 -c 'print((" U64 "*" U64 ")%%" U64 ")'", a,b,m);
 	py_mod(right);
 	uint64_t myres = multimod(a, b, m);
-//	sprintf(mymod, "%lu", myres);
-//	if (strcmp(right, mymod) != 0) {
-//		printf("Failure!");
-//		printf("right: %s\nmymod: %s\n", right, mymod);
-//	} 
-//		printf("(%lu * %lu) %% %lu \nright: %s\nmymod: %s\n\n", a, b, m, right, mymod);
+	//	sprintf(mymod, "%lu", myres);
+	//	if (strcmp(right, mymod) != 0) {
+	//		printf("Failure!");
+	//		printf("right: %s\nmymod: %s\n", right, mymod);
+	//	} 
+	//		printf("(%lu * %lu) %% %lu \nright: %s\nmymod: %s\n\n", a, b, m, right, mymod);
 	printf("(" U64 " * " U64 ") mod " U64 " = %s\n\n", a, b, m, right);
 	printf("(" U64 " * " U64 ") mod " U64 " = " U64 "\n\n", a, b, m, myres);
 }
 
 int main() {
-	uint64_t a, b, m; 
-	srand((unsigned)time(NULL));
-	for (int k = 0; k < NUM ; k++) {
-		a = rand()*9999 - 10000;
-		b = rand()*9999 - 10000;
-		m = rand()*9999 - 10000;
-		test(a, b, m);
-	}
-	test(-2ULL, -2ULL, -1ULL); // should be 1
+	/* uint64_t a, b, m; */ 
+	/* srand((unsigned)time(NULL)); */
+	/* for (int k = 0; k < NUM ; k++) { */
+	/* 	a = rand()*9999 - 10000; */
+	/* 	b = rand()*9999 - 10000; */
+	/* 	m = rand()*9999 - 10000; */
+	/* 	test(a, b, m); */
+	/* } */
+	test(-2ULL, -2ULL, 19);
+	/* test(-2ULL, -2ULL, -1ULL); // should be 1 */
 }
 
