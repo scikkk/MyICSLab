c783f9840811f2312a738876dc63c3b3b5d76493
tracer-nju <tracer@nju.edu.cn>
2021-11-05 19:24:13 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 84dde68a71c84ff1b904a9c70f2a25fe     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  19:24:13 up 51 min,  1 user,  load average: 0.01, 0.03, 0.03

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -1,26 +1,37 @@
 #include "asm.h"
 #include <string.h>
 
+#define 
+
 int64_t asm_add(int64_t a, int64_t b) {
-  return a + b;
+	int64_t ret;
+	asm(
+			"leal (%[a],%[b],1), %[ret]"
+			: [ret] "+r"(ret)
+			: [a] "r"(a), [b] "r"(b)
+	   );
+	return ret;
 }
 
 int asm_popcnt(uint64_t x) {
-  int s = 0;
-  for (int i = 0; i < 64; i++) {
-    if ((x >> i) & 1) s++;
-  }
-  return s;
+	int s = 0;
+	for (int i = 0; i < 64; i++) {
+		if ((x >> i) & 1) s++;
+	}
+	/* asm ( */
+
+	/* ) */
+	return s;
 }
 
 void *asm_memcpy(void *dest, const void *src, size_t n) {
-  return memcpy(dest, src, n);
+	return memcpy(dest, src, n);
 }
 
 int asm_setjmp(asm_jmp_buf env) {
-  return setjmp(env);
+	return setjmp(env);
 }
 
 void asm_longjmp(asm_jmp_buf env, int val) {
-  longjmp(env, val);
+	longjmp(env, val);
 }
