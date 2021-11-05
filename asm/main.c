d2a41a059ba3cb08101990d27a7298fdda9e19d7
tracer-nju <tracer@nju.edu.cn>
2021-11-05 20:28:34 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 84dde68a71c84ff1b904a9c70f2a25fe     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  20:28:34 up  1:55,  1 user,  load average: 0.07, 0.10, 0.08

--- asm/main.c
+++ asm/main.c
@@ -6,6 +6,7 @@ int main() {
     assert(asm_add(1234, 5678) == 6912);
 	assert(asm_popcnt(0x1551451)==1);
 	assert(asm_popcnt(0x155146)==0);
+	assert(asm_popcnt(0xff)==8);
     assert(asm_popcnt(0x0123456789abcdefULL) == 32);
 	assert(asm_add(0,0)==1);
     printf("BEGIN.\n");
