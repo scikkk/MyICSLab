5b754d4051b92883d2daedf7caa57b28e29bd2f5
tracer-nju <tracer@nju.edu.cn>
2021-11-05 20:03:58 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 84dde68a71c84ff1b904a9c70f2a25fe     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-38-generic       Architecture: x86-64  20:03:58 up  1:30,  1 user,  load average: 0.00, 0.03, 0.06

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -15,8 +15,8 @@ int64_t asm_add(int64_t a, int64_t b) {
 int asm_popcnt(uint64_t x) {
 	int sum = 0;
 	asm ( "movql %[x] %%eax;"
-			"andl $0x1, %eax;"
-			"addl %eax %[s];"
+			"andl $0x1, %%eax;"
+			"addl %%eax %[s];"
 		"shrq $0x1 %[x]"
 		:[s] "=r"(sum),[x] "=r"(x)
 		:
