701a4b15d8b3ce4c152b41cb12c2a5e9462ae545
tracer-nju <tracer@nju.edu.cn>
2021-11-13 16:45:48 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: e20040228f3a423b92ccce8e7177fe57     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  16:45:48 up 11 min,  1 user,  load average: 0.15, 0.15, 0.14

--- asm/asm.h
+++ asm/asm.h
@@ -17,5 +17,5 @@ typedef struct {
 int64_t asm_add(int64_t a, int64_t b);
 int     asm_popcnt(uint64_t x);
 void   *asm_memcpy(void *dest, const void *src, size_t n);
-int     asm_setjmp(asm_jmp_buf env);
-void    asm_longjmp(asm_jmp_buf env, int val);
+int     asm_setjmp(asm_jmp_buf *env);
+void    asm_longjmp(asm_jmp_buf *env, int val);
