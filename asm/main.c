df40245c95e3e91b76f81d10cd78257cf2828f7c
tracer-nju <tracer@nju.edu.cn>
2021-11-13 13:25:25 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: ddf2366ace9a40cda9336576642fbb8b     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  13:25:25 up 9 min,  1 user,  load average: 0.00, 0.14, 0.14

--- asm/main.c
+++ asm/main.c
@@ -3,16 +3,16 @@
 #include <stdio.h>
 
 int main() {
-	printf("\n\nBEGIN.\n");
+	/* printf("\n\nBEGIN.\n"); */
 	asm_jmp_buf buf;
 	int r = asm_setjmp(buf);
 	if (r == 0) {
-		printf("Enter first branch.\n");
+		/* printf("Enter first branch.\n"); */
 		asm_longjmp(buf, 123);
 	} else {
-		printf("Enter second branch.\n");
+		/* printf("Enter second branch.\n"); */
 		assert(r == 123);
-		printf("PASSED.\n");
+		/* printf("PASSED.\n"); */
 	}
-	printf("END.\n");
+	/* printf("END.\n"); */
 }
