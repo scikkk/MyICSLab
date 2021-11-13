c6365d156ff15ee68617903ebd4959fd4b4827f2
tracer-nju <tracer@nju.edu.cn>
2021-11-13 11:31:59 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: d0c60969d078480286ef3399f3228ef7     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  11:31:59 up 58 min,  1 user,  load average: 0.17, 0.14, 0.10

--- asm/main.c
+++ asm/main.c
@@ -12,8 +12,8 @@ int main() {
   /*   assert(r == 123); */
   /*   printf("PASSED.\n"); */
   /* } */
-  char a[] = "123456";
-  char b[] = "000000";
+  char a[] = "wangke2002";
+  char b[] = "00000000000000";
   asm_memcpy(b, a, 6);
   printf("a:%s\nb:%s\n", a, b);
     printf("END.\n");
