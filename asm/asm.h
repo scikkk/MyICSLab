5a9eb34d4524f76f79c0aab5a428dbaf9529fe5f
tracer-nju <tracer@nju.edu.cn>
2021-11-13 17:45:33 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: e20040228f3a423b92ccce8e7177fe57     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  17:45:33 up  1:11,  1 user,  load average: 0.00, 0.09, 0.09

--- asm/asm.h
+++ asm/asm.h
@@ -7,17 +7,6 @@
 
 typedef long long int asm_jmp_buf[8]; 
 
-/* typedef struct { */
-/* 	unsigned long eax; */
-/* 	unsigned long ebx; */
-/* 	unsigned long ecx; */
-/* 	unsigned long edx; */
-/* 	unsigned long edi; */
-/* 	unsigned long esi; */
-/* 	unsigned long esp; */
-/* 	unsigned long ebp; */
-/* 	unsigned long eip; */
-/* }asm_jmp_buf; */
 
 int64_t asm_add(int64_t a, int64_t b);
 int     asm_popcnt(uint64_t x);
