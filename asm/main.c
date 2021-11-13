2aa2389b32251a737a0900f6cfa1a403a43c07b3
tracer-nju <tracer@nju.edu.cn>
2021-11-13 11:30:48 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: d0c60969d078480286ef3399f3228ef7     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  11:30:48 up 57 min,  1 user,  load average: 0.12, 0.11, 0.09

--- asm/main.c
+++ asm/main.c
@@ -6,11 +6,14 @@ int main() {
 	printf("\n\nBEGIN.\n");
   asm_jmp_buf buf;
   int r = asm_setjmp(buf);
-  if (r == 0) {
-    asm_longjmp(buf, 123);
-  } else {
-    assert(r == 123);
-    printf("PASSED.\n");
-  }
+  /* if (r == 0) { */
+  /*   asm_longjmp(buf, 123); */
+  /* } else { */
+  /*   assert(r == 123); */
+  /*   printf("PASSED.\n"); */
+  /* } */
+  char a[] = "123456";
+  char b[] = "000000";
+  asm_memcpy(b, a, 6);
     printf("END.\n");
 }
