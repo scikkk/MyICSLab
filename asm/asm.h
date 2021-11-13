8a72660f222be05e96208aa8793bba51d87ba662
tracer-nju <tracer@nju.edu.cn>
2021-11-13 09:45:41 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: d0c60969d078480286ef3399f3228ef7     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  09:45:41 up 6 min,  1 user,  load average: 0.03, 0.16, 0.09

--- asm/asm.h
+++ asm/asm.h
@@ -1,10 +1,22 @@
 #include <stdint.h>
 #include <stddef.h>
 
-#define asm_jmp_buf jmp_buf
+/* #define asm_jmp_buf jmp_buf */
+
+struct asm_jmp_buf {
+	void* rsv; // 0(eax)
+	void* ebx; // 4
+	void* ecx; // 8
+	void* edx; // 12
+	void* esi; // 16
+	void* edi; // 20
+	void* ebp; // 24
+	void* esp; // 28
+	void* eip; // 32
+};
 
 int64_t asm_add(int64_t a, int64_t b);
 int     asm_popcnt(uint64_t x);
 void   *asm_memcpy(void *dest, const void *src, size_t n);
-int     asm_setjmp(asm_jmp_buf env);
-void    asm_longjmp(asm_jmp_buf env, int val);
+int     asm_setjmp(struct asm_jmp_buf env);
+void    asm_longjmp(struct asm_jmp_buf env, int val);
