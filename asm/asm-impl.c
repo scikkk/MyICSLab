c9b7c559e3b831f887d727f0912e5ee4f1625ba8
tracer-nju <tracer@nju.edu.cn>
2021-11-13 13:42:18 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: ddf2366ace9a40cda9336576642fbb8b     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  13:42:18 up 26 min,  1 user,  load average: 0.10, 0.10, 0.09

--- asm/asm-impl.c
+++ asm/asm-impl.c
@@ -51,16 +51,16 @@ void *asm_memcpy(void *dest, const void *src, size_t n) {
 
 int asm_setjmp(asm_jmp_buf env) {
 	asm(
-			"mov %%rsp  , %%rax;"
-			"mov %%rbx    , (%%rax);"
-			"mov %%rsi    , 4(%%rax);"
-			"mov %%rdi    , 8(%%rax);"
-			"mov %%rbp    , 12(%%rax);"
+			"mov 4(%%rsp)  , %%rax;"
+			"mov %%ebx    , (%%rax);"
+			"mov %%esi    , 4(%%rax);"
+			"mov %%edi    , 8(%%rax);"
+			"mov %%ebp    , 12(%%rax);"
 
 			"lea 4(%%rsp) , %%rcx;"
-			"mov %%rcx    , 16(%%rax);"
+			"mov %%ecx    , 16(%%rax);"
 			"mov (%%rsp)  , %%rcx;"
-			"mov %%rcx    , 20(%%rax);"
+			"mov %%ecx    , 20(%%rax);"
 			: 
 			:
 			: "ecx", "rax"
