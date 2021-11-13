375285934538fd2653984ed185f1517284389633
tracer-nju <tracer@nju.edu.cn>
2021-11-13 16:35:54 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: e20040228f3a423b92ccce8e7177fe57     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  16:35:54 up 1 min,  1 user,  load average: 1.02, 0.55, 0.21

--- asm/main.c
+++ asm/main.c
@@ -3,33 +3,18 @@
 #include <stdio.h>
 
 int main() {
-	/* printf("\n\nBEGIN.\n"); */
+	printf("\n\nBEGIN.\n");
 	asm_jmp_buf buf;
 	int r = asm_setjmp(buf);
 	printf("r:%d\n", r);
-	/* asm_longjmp(buf, 123); */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	/* if (r == 0) { */
-	/* 	printf("Enter first branch.\n"); */
-	/* 	asm_longjmp(buf, 123); */
-	/* 	printf("End first branch.\n"); */
-	/* } else { */
-	/* 	printf("Enter second branch.\n"); */
-	/* 	assert(r == 123); */
-	/* 	printf("PASSED.\n"); */
-	/* } */
-	/* printf("END.\n"); */
+	if (r == 0) {
+		printf("Enter first branch.\n");
+		asm_longjmp(buf, 123);
+		printf("End first branch.\n");
+	} else {
+		printf("Enter second branch.\n");
+		assert(r == 123);
+		printf("PASSED.\n");
+	}
+	printf("END.\n");
 }
