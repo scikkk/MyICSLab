0ed81664dd7c7f7a99764816e748d3e9d870f902
tracer-nju <tracer@nju.edu.cn>
2021-11-26 19:25:22 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 4487b5781a8a49fcbf0ac7aa83770b8e     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  19:25:22 up  1:50,  1 user,  load average: 0.00, 0.00, 0.00

--- perftune/main.c
+++ perftune/main.c
@@ -4,7 +4,7 @@
 int *sieve(int n);
 
 int main() {
-  int *primes = sieve(10000000);
+  int *primes = sieve(999999);
   for (int *cur = &primes[9999990], i = 0; *cur; cur++, i++) {
     printf("%8d", *cur);
     if (i % 8 == 7 || !*(cur + 1)) printf("\n");
