e7798c52d460d65fdc29991006c6a1dec11b2fca
tracer-nju <tracer@nju.edu.cn>
2021-11-26 17:53:41 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 4487b5781a8a49fcbf0ac7aa83770b8e     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  17:53:41 up 18 min,  1 user,  load average: 0.02, 0.06, 0.08

--- perftune/main.c
+++ perftune/main.c
@@ -9,4 +9,16 @@ int main() {
     printf("%8d", *cur);
     if (i % 8 == 7 || !*(cur + 1)) printf("\n");
   }
+  printf("wk\n");
+  	for(int k = 100, count = 0; k < MAXSIZE; k++){
+		bool newline = false;
+		if(is_prime[k]){
+			newline = true;
+			count++;
+			cout << k << ' ';
+		}
+		if (newline && count % 10 == 0){
+			cout << '\n';
+		}
+	}
 }
