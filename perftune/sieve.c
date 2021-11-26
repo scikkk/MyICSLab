afa16a17dafaf2318aafa8ea61aea916b98c4b63
tracer-nju <tracer@nju.edu.cn>
2021-11-26 18:45:11 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 4487b5781a8a49fcbf0ac7aa83770b8e     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-40-generic       Architecture: x86-64  18:45:11 up  1:10,  1 user,  load average: 0.06, 0.07, 0.01

--- perftune/sieve.c
+++ perftune/sieve.c
@@ -9,34 +9,35 @@ static bool is_prime[N];
 static int  primes[N];
 
 int *sieve(int n) {
-  assert(n + 1 < N);
-  /* for (int i = 0; i <= n; i++) */
-  /*   is_prime[i] = true; */
+	assert(n + 1 < N);
 
-  /* for (int i = 2; i <= n; i++) { */
-  /*   for (int j = i + i; j <= n; j += i) { */
-  /*     is_prime[j] = false; */
-  /*   } */
-  /* } */
+	/* for (int i = 2; i <= n; i++) { */
+	/*   for (int j = i + i; j <= n; j += i) { */
+	/*     is_prime[j] = false; */
+	/*   } */
+	/* } */
 
-  /* int *p = primes; */
-  /* for (int i = 2; i <= n; i++) */
-  /*   if (is_prime[i]) { */
-  /*     *p++ = i; */
-  /*   } */
-  /* *p = 0; */
-  /* return primes; */
-  	int primesize = 0;
-    memset(is_prime,1,sizeof(is_prime));
-    is_prime[1]=false;
-    for(int i=2;i<=n;i++)
-    {
-        if(is_prime[i])primes[primesize++]=i;
-         for(int j=0;j<primesize&&i*primes[j]<=n;j++)
-         {
-            is_prime[i*primes[j]]=false;
-            if(i%primes[j]==0)break;
-        }
-    }
-    return primes;
+	/* int *p = primes; */
+	/* for (int i = 2; i <= n; i++) */
+	/*   if (is_prime[i]) { */
+	/*     *p++ = i; */
+	/*   } */
+	/* *p = 0; */
+	/* return primes; */
+	int primesize = 0;
+	/* memset(is_prime,1,sizeof(is_prime)); */
+
+	for (int i = 0; i <= n; i++)
+		is_prime[i] = true;
+	is_prime[1]=false;
+	for(int i=2;i<=n;i++)
+	{
+		if(is_prime[i])primes[primesize++]=i;
+		for(int j=0;j<primesize&&i*primes[j]<=n;j++)
+		{
+			is_prime[i*primes[j]]=false;
+			if(i%primes[j]==0)break;
+		}
+	}
+	return primes;
 }
