2dafe38cca67414189d2a39cf26a0729c5ad6eae
tracer-nju <tracer@nju.edu.cn>
2021-12-04 16:29:06 +0800

    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e3e10734f8d84a3b93965bb75f646849            Boot ID: 54b3f47a6bb44596887e66ff0746cd24     Virtualization: vmware   Operating System: Ubuntu 21.04             Kernel: Linux 5.11.0-41-generic       Architecture: x86-64  16:29:06 up 35 min,  1 user,  load average: 0.00, 0.02, 0.02

--- cachesim/cache.c
+++ cachesim/cache.c
@@ -4,6 +4,16 @@
 void mem_read(uintptr_t block_num, uint8_t *buf);
 void mem_write(uintptr_t block_num, const uint8_t *buf);
 
+typedef uint32_t[16] Block;
+typedef struct{
+	Block data;
+	bool valid;
+} cache_line;
+
+cache_line *cache;
+static int lu = 0;
+
+
 static uint64_t cycle_cnt = 0;
 
 void cycle_increase(int n) { cycle_cnt += n; }
@@ -18,6 +28,8 @@ void cache_write(uintptr_t addr, uint32_t data, uint32_t wmask) {
 }
 
 void init_cache(int total_size_width, int associativity_width) {
+	cache = malloc(exp2(total_size_width)+exp2(total_size_width)/64);
+	lu = exp2(associativity_width);
 }
 
 void display_statistic(void) {
